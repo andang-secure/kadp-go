664a4fbdc43d69720c3004380880a112799ef463
‘17612926360’ <1329959757@qq.com>
2023-10-07 17:18:09 +0800

    新增aes密文base64输出

--- encryption/aes.go
+++ encryption/aes.go
@@ -8,7 +8,7 @@ import (
 )
 
 // aesEncrypt 使用AES-CBC/NoPadding模式加密数据
-func aesEncrypt(plaintext []byte, key []byte, iv []byte) (string, error) {
+func aesEncrypt(plaintext []byte, key []byte) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
@@ -20,7 +20,7 @@ func aesEncrypt(plaintext []byte, key []byte, iv []byte) (string, error) {
 
 	ciphertext := make([]byte, len(paddedPlaintext))
 
-	iv = make([]byte, aes.BlockSize)
+	iv := make([]byte, aes.BlockSize)
 	// 可以使用随机生成的IV，或者使用固定的IV
 	// 如果使用固定的IV，请确保每次加密的IV都是唯一的
 	// 如果使用随机生成的IV，请确保在解密时将IV与密文一起传递
@@ -34,24 +34,29 @@ func aesEncrypt(plaintext []byte, key []byte, iv []byte) (string, error) {
 }
 
 // aseDecrypt  使用AES-CBC/NoPadding模式解密数据
-func aseDecrypt(ciphertext []byte, key []byte) ([]byte, error) {
+func aseDecrypt(ciphertext string, key []byte) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	iv := make([]byte, aes.BlockSize)
 	// 这里示例简化，将IV设置为密钥
 	copy(iv, key)
 
-	plaintext := make([]byte, len(ciphertext))
+	textByte, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	plaintext := make([]byte, len(textByte))
 
 	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(plaintext, ciphertext)
+	mode.CryptBlocks(plaintext, textByte)
 
 	// 去除填充数据
 	padding := int(plaintext[len(plaintext)-1])
 	plaintext = plaintext[:len(plaintext)-padding]
 
-	return plaintext, nil
+	return string(plaintext), nil
 }
