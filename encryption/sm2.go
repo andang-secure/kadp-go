e58b4f7911a8f62370dbb888bfdfe09883528d2a
‘17612926360’ <1329959757@qq.com>
2023-10-26 15:56:21 +0800

    新增SM2摘要哈希

--- encryption/sm2.go
+++ encryption/sm2.go
@@ -2,24 +2,63 @@ package encryption
 
 import (
 	"crypto/rand"
-	"encoding/base64"
-	"github.com/tjfoc/gmsm/sm2"
+	"fmt"
+	"github.com/go-irain/logger"
+	"github.com/tjfoc/gmsm/x509"
+	"log"
 	"math/big"
+
+	"encoding/base64"
+	"encoding/pem"
+	"github.com/tjfoc/gmsm/sm2"
 )
 
-func sm2GenerateKey() (*sm2.PublicKey, *sm2.PrivateKey, error) {
+func sm2GenerateKey() (string, string, error) {
 	privateKey, err := sm2.GenerateKey(rand.Reader)
 	if err != nil {
-		return nil, nil, err
+		return "", "", err
 	}
 
-	publicKey := &privateKey.PublicKey // 公钥
+	publicKeyDER, err := x509.MarshalSm2PublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return "", "", err
+	}
 
-	return publicKey, privateKey, nil
+	// 创建公钥的PEM块
+	publicKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyDER,
+	}
+	// 将公钥PEM块编码为字符串
+	encodedPublicKey := string(pem.EncodeToMemory(publicKeyBlock))
+
+	// 将私钥编码为PKCS1格式
+	privateKeyDER, err := x509.MarshalSm2PrivateKey(privateKey, nil)
+	if err != nil {
+		return "", "", err
+	}
+	// 创建私钥的PEM块
+	privateKeyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyDER,
+	}
+	// 将私钥PEM块编码为字符串
+	encodedPrivateKey := string(pem.EncodeToMemory(privateKeyBlock))
+
+	return encodedPublicKey, encodedPrivateKey, nil
 }
 
-func sm2PubEncrypt(pubKey *sm2.PublicKey, data []byte, mode int) (string, error) {
-	ciphertext, err := sm2.Encrypt(pubKey, data, rand.Reader, mode)
+func sm2PubEncrypt(pubKey string, plaintext string) (string, error) {
+
+	block, _ := pem.Decode([]byte(pubKey))
+	if block == nil {
+		logger.Error("解码PEM失败")
+		return "", fmt.Errorf("解码PEM失败")
+	}
+
+	pub, err := x509.ParseSm2PublicKey(block.Bytes)
+
+	ciphertext, err := sm2.EncryptAsn1(pub, []byte(plaintext), rand.Reader)
 	if err != nil {
 		return "", err
 	}
@@ -29,32 +68,83 @@ func sm2PubEncrypt(pubKey *sm2.PublicKey, data []byte, mode int) (string, error)
 	return base64Ciphertext, nil
 }
 
-func sm2PriDecrypt(priKey *sm2.PrivateKey, cipherText string, mode int) ([]byte, error) {
+func sm2PriDecrypt(priKey string, cipherText string) (string, error) {
 	decodeData, err := base64.StdEncoding.DecodeString(cipherText)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	ciphertext, err := sm2.Decrypt(priKey, decodeData, mode)
+	block, _ := pem.Decode([]byte(priKey))
+	if block == nil {
+		logger.Error("解码PEM失败")
+		return "", fmt.Errorf("解码PEM失败")
+	}
+
+	parsedKey, err := x509.ParsePKCS8PrivateKey(block.Bytes, nil)
+
 	if err != nil {
-		return nil, err
+		return "", err
+	}
+	ciphertext, err := sm2.DecryptAsn1(parsedKey, decodeData)
+	if err != nil {
+		return "", err
 	}
 
-	return ciphertext, nil
+	return string(ciphertext), nil
 }
 
-func sm2Sign(pri *sm2.PrivateKey, data []byte, uid []byte) (r, s *big.Int, err error) {
-	sign, b, err := sm2.Sm2Sign(pri, data, uid, rand.Reader)
-	if err != nil {
-		return nil, nil, err
+func sm2Sign(pubKey string, data []byte, uid []byte) (r, s string, err error) {
+
+	block, _ := pem.Decode([]byte(pubKey))
+	if block == nil {
+		log.Println("解码PEM失败")
+		return "", "", fmt.Errorf("解码PEM失败")
 	}
 
-	return sign, b, nil
+	parsedKey, err := x509.ParsePKCS8PrivateKey(block.Bytes, nil)
+	if err != nil {
+		return "", "", err
+	}
+	sign, b, err := sm2.Sm2Sign(parsedKey, data, uid, rand.Reader)
+	if err != nil {
+		return "", "", err
+	}
+	signBytes := sign.Bytes()
+	bBytes := b.Bytes()
+
+	rBase64 := base64.StdEncoding.EncodeToString(signBytes)
+	sBase64 := base64.StdEncoding.EncodeToString(bBytes)
+
+	return rBase64, sBase64, nil
 }
 
-func sm2Verify(pub *sm2.PublicKey, data []byte, uid []byte, r, s *big.Int) bool {
+func sm2Verify(pubKey string, data []byte, uid []byte, r, s string) (bool, error) {
 
-	isVerify := sm2.Sm2Verify(pub, data, uid, r, s)
+	block, _ := pem.Decode([]byte(pubKey))
+	if block == nil {
+		logger.Error("解码PEM失败")
+		return false, fmt.Errorf("解码PEM失败")
+	}
 
-	return isVerify
+	publicKeyObject, err := x509.ParseSm2PublicKey(block.Bytes)
+	if err != nil {
+		return false, err
+	}
+
+	rBytes, err := base64.StdEncoding.DecodeString(r)
+	if err != nil {
+		return false, err
+	}
+	sBytes, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return false, err
+	}
+
+	rInt := new(big.Int).SetBytes(rBytes)
+
+	sInt := new(big.Int).SetBytes(sBytes)
+
+	isVerify := sm2.Sm2Verify(publicKeyObject, data, uid, rInt, sInt)
+
+	return isVerify, nil
 }
--- test/KadpClient_test.go
+++ test/KadpClient_test.go
@@ -6,14 +6,15 @@ import (
 	"github.com/andang-security/kadp-go/encryption"
 	"github.com/andang-security/kadp-go/mode"
 	"github.com/andang-security/kadp-go/padding"
+	"github.com/go-irain/logger"
 	"log"
 	"testing"
 )
 
 func TestAdd(t *testing.T) {
 
-	url := "https://192.168.0.130:10060"
-	token := "epYu8UNoLOYNBJPYLVaTdCXCZvK7ku9leEyWZjA58DVqjJ8fLfbmO29T6Amusg45iR2WDsAbGgalED1iXD/rEF4ujA8UNuXIMi2PdptjHdhEB0Qlz5G6AbCAexXDhKJyET8IuRNyWQ9OjVmI59OmRdKlzNANAcoqNShrUQIppTRNf9YpB7bjFeprmHH0IqwDZi26QnFD+nYCrztHIr1sloKDBle6a6bjl5V0pl3psUkq81XPBvPYRmePABplQqOQObQq0+Wa7ftBGQefsODWf5CgEsGRUoePltKLRtHWiTUdDdm7v0bJljuhhzjlLKHajQLUnxq3Wi5BaMp39JOg3j8Fji/UhXSbi+XtUKLWQiepYjqQa7e1LONYpgkhX7V34HirVvaoTsw+sRhcR1ol0g=="
+	url := "https://192.168.0.190:8090"
+	token := "epYu8UNoLOYNBJPYLVaTdCXCZvK7ku9leEyWZjA58DVqjJ8fLfbmO29T6Amusg45iR2WDsAbGgalED1iXD/rEDpPYO1M/Hr6lfNuCmfL+Du6ijtN0EwQ4ph9Khbyk/RSj0uZTs4kQCe+Dg5Rq8zBS92LJmcxUNPJb/XKi36TIYSXjsQmmZtnkmyrC10i67uO59OF/Ea9t7AsoF78ytTjPagSJH4iI3zXYUzwhgtxwsybTlBCs1NjP3ht3VT2BvKo3HakFvinHnBseEwwTKT0HdfB0So1+6YPEikKis5ejs7Pyuh9rAIEiA0NmriDFtVvF9Hqq+wZn97ZE1n6ewCXAT0vqH8egm4KqDpxfdLME+4sy7nBU5bDG20HfYG7+7BsMb/c3+8Cq1TT8oynJbZCBg=="
 	myClient := encryption.NewKADPClient(url, token)
 
 	encrypt, err := myClient.FpeEncipher("151918123", algorithm.FF3, "12345678", 36, 16, "kadp112", 2, 6)
@@ -27,13 +28,13 @@ func TestAdd(t *testing.T) {
 	decipher, err := myClient.FpeDecipher(encrypt, algorithm.FF3, "12345678", 36, 16, "kadp112", 2, 6)
 	fmt.Println(decipher)
 
-	encipher, err := myClient.Encipher([]byte(asdas), algorithm.AES, mode.CBC, padding.NoPadding, 16, "kadp11211", "1234567897894561")
+	encipher, err := myClient.Encipher([]byte(asdas), algorithm.DES, mode.CBC, padding.NoPadding, 16, "awd1", "12345678")
 	if err != nil {
-		return
+		fmt.Println(err)
 	}
 	fmt.Println("AES密文" + encipher)
 
-	plaintext, err := myClient.Decipher(encipher, algorithm.AES, mode.CBC, padding.NoPadding, 16, "kadp11211", "1234567897894561")
+	plaintext, err := myClient.Decipher(encipher, algorithm.DES, mode.CBC, padding.NoPadding, 16, "awd1", "12345678")
 	fmt.Println(plaintext)
 
 	if err != nil {
@@ -42,4 +43,37 @@ func TestAdd(t *testing.T) {
 	fmt.Println("解密" + decipher)
 	log.Println(decipher)
 
+	pub, pri, err := myClient.AsymmetricKeyPair(algorithm.RSA)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("公钥：", pub)
+	fmt.Println("私钥：", pri)
+
+	publicEncrypt, err := myClient.AsymmetricPubEncrypt("文档是我的", algorithm.RSA, pub)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("公钥加密过后密文：", publicEncrypt)
+
+	decrypt, err := myClient.AsymmetricPriDecrypt(publicEncrypt, algorithm.RSA, pri)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("私钥解密：", decrypt)
+
+	r, s, err := myClient.SM2Signature("Wdswd", pri, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("签名:", r, s)
+
+	verify, err := myClient.SM2Verify("Wdswd", pub, r, s, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	logger.Debug("验签:", verify)
+
+	Hash := myClient.DigestEncrypt("wdad")
+	logger.Debug("摘要哈希：", Hash)
 }
