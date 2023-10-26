54ca57b8d8f8d94e42bbc528eca387bb70d76598
‘17612926360’ <1329959757@qq.com>
2023-10-26 15:56:01 +0800

    SM4解析key错误修复

--- encryption/sm4.go
+++ encryption/sm4.go
@@ -5,24 +5,32 @@ import (
 	"github.com/tjfoc/gmsm/sm4"
 )
 
-func sm4CbcEncrypt(plaintext, key []byte) (string, error) {
-	cipherText, err := sm4.Sm4Cbc(key, plaintext, true)
+func sm4CbcEncrypt(plaintext []byte, key string) (string, error) {
+
+	decodeKey, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+	cipherText, err := sm4.Sm4Cbc(decodeKey, plaintext, true)
 
 	if err != nil {
 		return "", err
 	}
-
 	cipherTextBase64 := base64.StdEncoding.EncodeToString(cipherText)
 	return cipherTextBase64, nil
 }
 
-func sm4CbcDecrypt(plaintext string, key []byte) (string, error) {
+func sm4CbcDecrypt(plaintext, key string) (string, error) {
 
+	decodeKey, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
 	plaintextDecode, err := base64.StdEncoding.DecodeString(plaintext)
 	if err != nil {
 		return "", err
 	}
-	cipherText, err := sm4.Sm4Cbc(key, plaintextDecode, true)
+	cipherText, err := sm4.Sm4Cbc(decodeKey, plaintextDecode, true)
 
 	if err != nil {
 		return "", err
